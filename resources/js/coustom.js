470527161d36ab5d37fe1417a9fb25e758936094
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2021-01-21 15:57:21 +0600

    DO navbar sticky

--- resources/js/coustom.js
+++ resources/js/coustom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+
+$('.js--service-section').waypoint(function(direction){
+    if(direction == "down"){
+        $("nav").addClass("sticky");
+    }else{
+        $("nav").removeClass("sticky");
+    }
+})
+    // add mixitup on Portfolio section
     const mixer = mixitup('.portfolioContainer');
 })
