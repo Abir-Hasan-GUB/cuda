c50bafbb4ba27ca29119b06111f13d62080a2b89
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2021-01-21 23:02:42 +0600

    Alhamdulillah done ! Site ready for live

--- resources/js/coustom.js
+++ resources/js/coustom.js
@@ -10,3 +10,25 @@ $('.js--service-section').waypoint(function(direction){
     // add mixitup on Portfolio section
     const mixer = mixitup('.portfolioContainer');
 })
+
+// Mobile Nav
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
+
+// coustom scroll behaviour working here
+$("a").on('click', function(event){
+    if(this.hash !== ""){
+        event.preventDefault();
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().scrollTop
+        }, 1000, function(){
+            window.location.hash = hash;
+        });
+    }
+});
---
+++ resources/js/dist/coustom.dev.js
@@ -0,0 +1,34 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.js--service-section').waypoint(function (direction) {
+    if (direction == "down") {
+      $("nav").addClass("sticky");
+    } else {
+      $("nav").removeClass("sticky");
+    }
+  }); // add mixitup on Portfolio section
+
+  var mixer = mixitup('.portfolioContainer');
+}); // Mobile Nav
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+} // coustom scroll behaviour working here
+
+
+$("a").on('click', function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().scrollTop
+    }, 1000, function () {
+      window.location.hash = hash;
+    });
+  }
+});
